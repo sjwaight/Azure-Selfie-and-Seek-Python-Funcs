99e2e7b5b382206fc3a5c73942b3cc351239fdcf
Simon Waight <siwaight@microsoft.com>
2019-08-11 21:30:56 +1000

    Added ActiveGame; remove anonymous access.

---
+++ ActiveGame/__init__.py
@@ -0,0 +1,29 @@
+import logging
+import os
+import json
+import datetime
+
+import azure.functions as func
+
+# Azure Table Storage import and config
+from azure.storage.blob import BlockBlobService, ContentSettings
+from azure.storage import CloudStorageAccount
+from azure.storage.table import TableService, Entity, EntityProperty, EdmType
+
+table_service = TableService(os.environ.get("STORAGE_ACCOUNT"), os.environ.get("STORAGE_KEY"))
+
+def datetimeconverter(o):
+    if isinstance(o, datetime.datetime):
+        return o.__str__()
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+
+    try:                
+        # Read game configuration
+        game_config = table_service.get_entity(os.environ.get("GAME_CONFIG_TABLENAME"), os.environ.get("GAME_CONFIG_PARTITIONKEY"), os.environ.get("GAME_CONFIG_ROWKEY"))
+
+        return func.HttpResponse(json.dumps(game_config, default=datetimeconverter))
+       
+    except Exception as e:
+        logging.critical(e)
+        return func.HttpResponse("There was an error - please check logs", status_code=500)
